10ad2da979ef78f10a9d9da5ccd8c41aeef90d71
Ihsan Bille <117181035+IhsanYnov@users.noreply.github.com>
2023-02-22 11:39:55 +0100

    Create Test.php

---
+++ Test.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>PHP Page web IHSAN</title>
+    </head>
+    <body>
+        <?php 
+            echo '<p>Test</p>';
+            phpinfo(); ?>
+    </body>
+</html>
